21ec0d9339a7977c2dbc369d392a838ba00adb3a
DayTroy <shadowyguy5@gmail.com>
2024-01-05 22:47:52 +0300

    Added validation

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-
+  form = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  constructor() {}  
+  submit() {
+    if (this.form.invalid) return
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    }
+  }
 }
