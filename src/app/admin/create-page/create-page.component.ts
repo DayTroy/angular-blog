f0af067ab583623168b637d9c137c0e4cc213057
DayTroy <shadowyguy5@gmail.com>
2024-01-06 18:14:34 +0300

    Added create post form

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../shared/interfaces';
 
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html',
   styleUrl: './create-page.component.scss'
 })
-export class CreatePageComponent {
+export class CreatePageComponent implements OnInit{
 
+  form = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    text: new FormControl('', [Validators.required]),
+    author: new FormControl('', [Validators.required]),
+  });
+
+  submit() {
+    if (this.form.invalid) return
+    const post: Post = {
+      title: this.form.value.title,
+      author: this.form.value.author,
+      text: this.form.value.text,
+      date: new Date(),
+    }
+  }
+
+  constructor(){}
+  ngOnInit(){
+    
+  }
 }
--- src/app/admin/shared/interfaces.ts
+++ src/app/admin/shared/interfaces.ts
@@ -4,6 +4,14 @@ export interface User {
     returnSecureToken?: boolean
 }
 
+export interface Post {
+    id?: string | null | undefined;
+    title: string | null | undefined;
+    text: string | null | undefined;
+    author: string | null | undefined;
+    date: Date;
+}
+
 export interface FbAuthResponse {
     idToken: string
     expiresIn: string
