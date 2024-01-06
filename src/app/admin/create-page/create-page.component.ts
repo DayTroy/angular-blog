c494fb8c1f6151bd05e25cdd05b334fdc4bc9977
DayTroy <shadowyguy5@gmail.com>
2024-01-06 22:32:30 +0300

    Installed text edit plugin - quill

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -23,7 +23,8 @@ export class CreatePageComponent implements OnInit{
       text: this.form.value.text,
       date: new Date(),
     }
-  }
+    console.log(post)
+  } 
 
   constructor(){}
   ngOnInit(){
