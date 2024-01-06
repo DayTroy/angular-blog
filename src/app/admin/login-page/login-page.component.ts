e8fb21e39cb3881cb776bd3ce7d3e8b8371cd786
DayTroy <shadowyguy5@gmail.com>
2024-01-06 18:01:18 +0300

    Added guard service

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,15 +1,16 @@
 import { AuthService } from './../shared/services/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '../shared/interfaces';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit{
+
   form = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
     password: new FormControl('', [
@@ -18,10 +19,19 @@ export class LoginPageComponent {
     ]),
   });
   submitted = false
+  message: string = ' '
+  
+
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}  
+  ngOnInit(){
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) this.message = 'Пожалуйста, введите данные'
+    })
+  }
   submit() {
     if (this.form.invalid) return
     this.submitted = true
