2a795a6f50e33e8b35ef6f7d7e6fbbe3039bd27a
DayTroy <shadowyguy5@gmail.com>
2024-01-07 18:10:56 +0300

    Registered ru localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { SharedModule } from './shared/shared.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './shared/auth.interceptor';
 
+import { registerLocaleData } from '@angular/common';
+import ruLocale from "@angular/common/locales/ru";
+ 
+
+registerLocaleData(ruLocale, 'ru')
 
 const INTECEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
