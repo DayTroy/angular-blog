6bc840633c6eb86b511f315099c35c04bf6eface
DayTroy <shadowyguy5@gmail.com>
2024-01-07 20:27:06 +0300

    Added post cards output

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostsService } from './../shared/posts.service';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../admin/shared/interfaces';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
 
+  posts$: Observable<Post[]> = new Observable<Post[]>()
+
+  constructor(private postsService: PostsService) {}
+  ngOnInit() {
+    this.posts$ = this.postsService.getAll()
+  }
 }
--- src/app/shared/components/post/post.component.ts
+++ src/app/shared/components/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../../../admin/shared/interfaces';
 
 @Component({
   selector: 'app-post',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
-
+  @Input() post: Post = <Post>{}
 }
